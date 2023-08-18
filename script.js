2cd705e51007ec69b810a5207edc15944e3d6940
object-ions <moshikoatia@gmail.com>
2023-08-18 10:14:04 -0600

    add email validator

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
 //Add event listener
 form.addEventListener('submit', (e) => {
     e.preventDefault();
